e7288c73e34c7e0098f10fced87a6bc09c27997a
Blair Learn <blairlearn@users.noreply.github.com>
2016-10-21 21:56:37 +0000

    Create zip file. Begin laying groundwork for git release.

--- release-build.sh
+++ release-build.sh
@@ -5,11 +5,20 @@ echo Creating Release Build.
 # Create temporary location for publishing output
 TMPDIR=`mktemp -d` || exit 1
 
+# Main project directory
+PROJECT_DIR=`pwd`
+
 # Publish to temporary location.
 dotnet restore
 dotnet publish -o $TMPDIR
 
-# Create archive for uploading to GitHub
+# Create archive for uploading to GitHub.  Creating it in the
+# Publishing folder eliminates the parent directory path.
 echo "Creating release archive"
-zip -r project-release.zip $TMPDIR
+cd $TMPDIR
+zip -r project-release.zip .
+cd $PROJECT_DIR
 
+# Export token and enterprise api to enable github-release tool
+export GITHUB_TOKEN=$$$$$$$$$$$$
+export GITHUB_API=https://git.{your domain}.com/api/v3 # needed only for enterprise
