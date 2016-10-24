00f65f209fbb7fdfe9fd0eade68a85b9ccf8067e
Blair Learn <blairlearn@users.noreply.github.com>
2016-10-24 16:56:26 -0400

    Setup to use GitHub token for releases.

--- release-build.sh
+++ release-build.sh
@@ -1,9 +1,15 @@
 # This is the template for a release build.
+# Command Line Parameters:
+#   $1 - GitHub token for creating a release and uploading artifacts.
 
 echo Creating Release Build.
 
 # Create temporary location for publishing output
 TMPDIR=`mktemp -d` || exit 1
+
+# Export token and enterprise api to enable github-release tool
+export GITHUB_TOKEN=$1
+# export GITHUB_API=https://git.{your domain}.com/api/v3 # needed only for enterprise
 
 # Main project directory
 PROJECT_DIR=`pwd`
@@ -18,7 +24,3 @@ echo "Creating release archive"
 cd $TMPDIR
 zip -r project-release.zip .
 cd $PROJECT_DIR
-
-# Export token and enterprise api to enable github-release tool
-export GITHUB_TOKEN=$$$$$$$$$$$$
-export GITHUB_API=https://git.{your domain}.com/api/v3 # needed only for enterprise
